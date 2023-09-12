0820c3c7a7d723c78d2ea64ce33aeca8008e83e0
JackSoer <kirillrotko1@gmail.com>
2023-09-12 14:12:32 +0300

    Added all phone codes

--- src/components/phoneInput/PhoneInput.tsx
+++ src/components/phoneInput/PhoneInput.tsx
@@ -1,6 +1,8 @@
-import { ChangeEvent, ReactElement, useState } from 'react';
+import { ReactElement, useState } from 'react';
 import UserType from '../../types/UserType.type';
 import './phoneInput.scss';
+import { default as PhoneInputElement } from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
 
 type PhoneInputProps = {
   updateUserFields: (userNewFields: Partial<UserType>) => void;
@@ -11,43 +13,20 @@ const PhoneInput = ({
   updateUserFields,
   phone,
 }: PhoneInputProps): ReactElement => {
-  const [inputValue, setInputValue] = useState(phone || '+1');
-  const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(true);
+  const [inputValue, setInputValue] = useState(phone);
 
-  const formatPhoneNumber = (value: string): string => {
-    if (!value) return value;
+  const handleChange = (
+    // @ts-ignore
+    value: string,
+    // @ts-ignore
+    data: any,
+    // @ts-ignore
+    event: any,
+    formattedPhone: string
+  ) => {
+    setInputValue(formattedPhone);
 
-    const phoneNumber = value.replace(/^\+1|\D+/g, '');
-    const phoneLength = phoneNumber.length;
-
-    if (phoneLength < 4) {
-      return `+1 ${phoneNumber}`;
-    }
-    if (phoneLength < 7) {
-      return `+1 (${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
-    }
-
-    return `+1 (${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
-      3,
-      6
-    )}-${phoneNumber.slice(6, 10)}`;
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setIsValidPhoneNumber(true);
-
-    const formattedNumber = formatPhoneNumber(e.target.value);
-    setInputValue(formattedNumber);
-
-    updateUserFields({ phone: formattedNumber });
-  };
-
-  const handleValidation = (phoneNumber: string) => {
-    if (phoneNumber.length !== 17) {
-      setIsValidPhoneNumber(false);
-    } else {
-      setIsValidPhoneNumber(true);
-    }
+    updateUserFields({ phone: formattedPhone });
   };
 
   return (
@@ -55,22 +34,18 @@ const PhoneInput = ({
       <label htmlFor="phone" className="phone-label">
         Phone
       </label>
-      <input
-        type="tel"
-        name="phone"
-        id="phone"
-        required
+      <PhoneInputElement
+        country="us"
         value={inputValue}
-        onChange={(e) => handleChange(e)}
-        onBlur={() => handleValidation(inputValue)}
-        placeholder="+1 (xxx) xxx-xxxx"
-        className="phone-input"
+        onChange={handleChange}
+        inputClass="phone-input"
+        inputProps={{
+          required: true,
+          name: 'phone',
+          id: 'phone',
+          type: 'tel',
+        }}
       />
-      {!isValidPhoneNumber && (
-        <span className="error">
-          Phone should be in +1 (xxx) xxx-xxxx format
-        </span>
-      )}
     </div>
   );
 };
